3c2951a11aef393b7df49600c78faddc455035eb
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-08-27 11:01:36 +0000

    Update connect.js

--- connect.js
+++ connect.js
@@ -6,6 +6,24 @@ const mqtt = require("mqtt");
 // middlewares
 app.use(express.json());
 
+// Get Request
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+// Post Request
+app.post("/connect", async (req, res) => {
+  const { host, port, clientId } = req.body;
+  const connectUrl = `mqtt://${host}:${port}`;
+  const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
+    reconnectPeriod: 1000,
+  });
+  await client.on("connect", () => {
+    console.log("Connected");
+  });
 });
