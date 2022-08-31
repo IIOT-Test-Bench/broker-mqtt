d9ec3d5499108999e82a2f0c3a2d33c82b245a13
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-08-31 21:57:56 +0000

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const Port = process.env.PORT || 3000;
 const mqtt = require("mqtt");
+const { emma } = require("./config");
 
 // middlewares
 app.use(express.json());
@@ -12,60 +13,8 @@ app.get("/", (req, res) => {
 });
 
 // Post Request
-app.get("/connect", async (req, res) => {
-  const { host, port, clientId } = req.body;
-  const connectUrl = `mqtt://${host}:${port}`;
-  const client = await mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
-});
 
-// Publish
-app.post("/publish", (req, res) => {
-  const { topic, message } = req.body;
-  const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
-
-  client.on("connect", () => {
-    client.publish(topic, message);
-    console.log("Message sent!", message);
-  });
-});
-
-// Subscribe
-app.post("/subscribe", (req, res) => {
-  const { topic } = req.body;
-  const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
-
-  client.on("connect", () => {
-    client.subscribe(topic, () => {
-      console.log(`Subscribe to topic ${topic}`);
-    });
-  });
-
-  client.on("message", (topic, message) => {
-    message = message.toString();
-    console.log(message);
-  });
-});
+emma;
 
 app.listen(Port, () => {
   console.log("App Running...");
