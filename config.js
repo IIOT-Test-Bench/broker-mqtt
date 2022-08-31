51a619fa1d3d476213f0b689083569713f5a8b2c
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-08-31 22:38:53 +0000

    update

--- config.js
+++ config.js
@@ -3,14 +3,11 @@ const host = "broker.emqx.io";
 const port = "1883";
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 const connectUrl = `mqtt://${host}:${port}`;
-module.exports.client = function () {
-  const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
-  return client;
-};
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: "emqx",
+  password: "public",
+  reconnectPeriod: 1000,
+});
--- connect.js
+++ connect.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const Port = process.env.PORT || 3000;
 const mqtt = require("mqtt");
-const { client } = require("./config");
+const host = "broker.emqx.io";
+const port = "1883";
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+const connectUrl = `mqtt://${host}:${port}`;
 
 // middlewares
 app.use(express.json());
@@ -15,19 +18,6 @@ app.get("/", (req, res) => {
 // Post Request
 // Connection
 app.get("/connect", (req, res) => {
-  const mqtt = require("mqtt");
-  const host = "broker.emqx.io";
-  const port = "1883";
-  const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
-  const connectUrl = `mqtt://${host}:${port}`;
-  const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
   client.on("connect", () => {
     console.log(`Connected`);
   });
