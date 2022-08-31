fd7913a403c4a7cfa29e8969580178c7dc0c9a13
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-08-31 21:51:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-{}
+const mqtt = require("mqtt");
+
+const connectUrl = `mqtt://${host}:${port}`;
+const client = await mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: "emqx",
+  password: "public",
+  reconnectPeriod: 1000,
+});
