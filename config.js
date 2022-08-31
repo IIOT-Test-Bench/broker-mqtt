e8de55978bba866fe51a883e8a51656bb212cfb5
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-08-31 21:52:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 const mqtt = require("mqtt");
+const host = "broker.emqx.io";
+const port = "1883";
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
 const connectUrl = `mqtt://${host}:${port}`;
 const client = await mqtt.connect(connectUrl, {
