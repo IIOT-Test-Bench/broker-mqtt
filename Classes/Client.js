5212a96417f7137c264753de8db7ae55f744d8a4
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-09-05 01:50:40 +0000

    Add a new connected client

--- Classes/Client.js
+++ Classes/Client.js
@@ -3,4 +3,11 @@ module.exports = class Client {
   static totalClients = 0;
   static publishedTopics = [];
   static subscribedTopics = [];
+
+  //Add a new connected client
+  static addClient(clientId, clientObj) {
+    this.clientList[clientId] = clientObj;
+    ++this.totalClients;
+    console.log(this.clientList[clientId]);
+  }
 };
