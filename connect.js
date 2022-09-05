e306f30385c5d313c9d6265309ac208fb97580fe
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-09-05 00:35:15 +0000

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,88 +1,102 @@
 const express = require("express");
 const app = express();
-const Port = process.env.PORT || 3001;
+const Port = process.env.PORT || 3000;
 const mqtt = require("mqtt");
+const Client = require("./Classes/Client");
 
-const cors = require('cors');
+const cors = require("cors");
 
 // middlewares
 app.use(express.json());
-app.use(cors({origin: true, credentials: true}));
 
 // Get Request
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-let clientobject = {};
-
 // Post Request
-app.post("/connect", async (req, res) => {
-  const { host, port, clientId, timeout, username, password } = req.body;
-  const connectUrl = `mqtt://${host}:${port}`;
-  const client = await mqtt.connect(connectUrl, {
-
+// Connection
+app.get("/connect", (req, res) => {
+  const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
-    connectTimeout: timeout,
-    username: username,
-    password: password,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
     reconnectPeriod: 1000,
   });
   client.on("connect", () => {
-    res.send({clientId, status: "connected", msg: "Successfully Connected"})
-    clientobject[clientId] = client;
-    // console.log(clientobject);
+    console.log(`Connected`);
   });
+  res.send("Broker Connected!");
 });
 
-//Close connection
-app.post("/disconnect", async (req, res) => {
-  const { clientId } = req.body;
-  const client = clientobject[clientId];
-  if(clientobject[clientId]){
-    await client.end();
-    res.send(`Client ${client.options.clientId} disconnected successfully`);
-    delete clientobject[clientId];
-    console.log(Object.keys(clientobject));
-  }else{
-    res.status(404).send("Sorry there was no client connection to be closed")
-  }
-  
+// Disconnect
+app.get("/disconnect", (req, res) => {
+  const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
+    reconnectPeriod: 1000,
+  });
+
+  client.end(() => {
+    console.log(`Broker Disconnected`);
+  });
+  res.send("Broker Disconnected");
 });
 
-    
 // published
-app.post("/publish", async (req, res) => {
-  const { clientId, topic, message } = req.body;
-  const client = clientobject[clientId];
+app.get("/publish", (req, res) => {
+  const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
+    reconnectPeriod: 1000,
+  });
 
-  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-    if (error) {
-      console.error(error);
-    }else{
-      console.log("Message sent!");
-      res.send(`${topic} published`);
-    }
-  }); 
+  client.on("connect", () => {
+    setInterval(function () {
+      let arr = Array(50).fill("some string");
+      for (let key in arr) {
+        if (key > 0) {
+          client.publish(`${key}`);
+        }
+      }
+    }, 3000);
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`Published Memory Usage ${Math.round(used * 100) / 100} MB`);
+  });
+  res.send("Topic Published");
 });
 
 // subscribe
-app.post("/subscribe", (req, res) => {
-  const { clientId, topic } = req.body;
-  const client = clientobject[clientId];
-  
-    client.subscribe(topic, () => {
-      console.log(`Subscribe to topic '${topic}'`);
-      res.send(`Subscribe to topic '${topic}'`)
-    });
+app.get("/subscribe", (req, res) => {
+  const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: "emqx",
+    password: "public",
+    reconnectPeriod: 1000,
+  });
+  client.on("connect", function () {
+    let arr = Array(800).fill("some string");
+    for (let key in arr) {
+      if (key > 0) {
+        client.subscribe(`${key}`);
+      }
+    }
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`Subscribe Memory Used ${Math.round(used * 100) / 100} MB`);
+  });
+  res.send("Subscribed to Topic");
 });
 
 app.listen(Port, () => {
   console.log("App Running...");
 });
-
-
-//Memory usage statistics
-// const used = process.memoryUsage().heapUsed / 1024 / 1024;
-// console.log(`Subscribe Memory Used ${Math.round(used * 100) / 100} MB`);
