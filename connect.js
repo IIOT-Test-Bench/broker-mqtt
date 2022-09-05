18b81bc69b1ad3875e6284833eae4d0e3f89e97c
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-09-05 01:02:26 +0000

    Publish api

--- connect.js
+++ connect.js
@@ -52,29 +52,9 @@ app.post("/disconnect", async (req, res) => {
 });
 
 // published
-app.get("/publish", (req, res) => {
-  const client = mqtt.connect(connectUrl, {
-    clientId,
-    clean: true,
-    connectTimeout: 4000,
-    username: "emqx",
-    password: "public",
-    reconnectPeriod: 1000,
-  });
-
-  client.on("connect", () => {
-    setInterval(function () {
-      let arr = Array(50).fill("some string");
-      for (let key in arr) {
-        if (key > 0) {
-          client.publish(`${key}`);
-        }
-      }
-    }, 3000);
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Published Memory Usage ${Math.round(used * 100) / 100} MB`);
-  });
-  res.send("Topic Published");
+app.post("/publish", async (req, res) => {
+  const { clientId, topic, message } = req.body;
+  const client = Client.getClient(clientId);
 });
 
 // subscribe
