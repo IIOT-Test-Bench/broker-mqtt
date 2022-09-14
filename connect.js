f575b08e989511e22bbb6f8c9832d1363cdfb0b2
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-09-14 23:34:38 +0000

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,8 +4,8 @@ const Port = process.env.PORT || 3001;
 const mqtt = require("mqtt");
 const Client = require("./Classes/Client");
 const Publisher = require("./Classes/Publisher");
-const {generateTopic} = require("./HelperFunctions/generateTopic"); //Get Random topic
-const {getRandomNumber} = require("./HelperFunctions/generateClientId"); //Get Random number
+const { generateTopic } = require("./HelperFunctions/generateTopic"); //Get Random topic
+const { getRandomNumber } = require("./HelperFunctions/generateClientId"); //Get Random number
 
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
@@ -22,7 +22,7 @@ const io = require("socket.io")(server, {
 });
 
 const cors = require("cors");
-//import subscriber class 
+//import subscriber class
 const Subscribe = require("./Classes/Subscriber");
 
 // middlewares
@@ -98,92 +98,110 @@ app.post("/subscribe", (req, res) => {
 
 //Simulation with websockets
 //Client used as the identity for the websocket client, MainClient to identify the main client connection to the broker
-  io.on('connection', client => {
+io.on("connection", (client) => {
+  let clientId = null;
 
-    let clientId = null;
+  client.emit("connectionStatus", {
+    isConnected: true,
+    status: "connected",
+    msg: "User connected",
+  });
+  client.on("clientId", (data) => {
+    console.log("Received client Id: " + data);
+    // const client = Client.getClient(data);
+    clientId = data;
+  });
 
-    client.emit('connectionStatus', {isConnected: true, status: "connected", msg:"User connected"});
-    client.on('clientId', data => { 
-      console.log("Received client Id: " + data);
-      // const client = Client.getClient(data);
-      clientId = data;
-     });
+  let samplePubs = [];
+  let sampleSubs = [];
+  let symbs = null;
+  let bytesRead = null;
+  let bytesWritten = null;
 
-     let samplePubs = [];
-     let sampleSubs = [];
-     let symbs = null;
-      let bytesRead = null;
-      let bytesWritten = null; 
+  client.on("startSimulation", (data) => {
+    //Receive parameters from the user
+    const { numOfPubs, pubInterval, pubTopicLevel, numOfSubs, subTopicLevel } =
+      data;
+    samplePubs = new Array(numOfPubs);
+    //Run publishing simulation
+    for (let i = 0; i < numOfPubs; i++) {
+      let pubTopic = generateTopic(4, pubTopicLevel);
+      samplePubs[i] = new Publisher(
+        `Publ ${i}`,
+        pubInterval,
+        clientId,
+        pubTopic
+      );
+    }
 
-
-     client.on('startSimulation', (data) => {
-      //Receive parameters from the user
-      const {numOfPubs, pubInterval, pubTopicLevel, numOfSubs, subTopicLevel} = data;
-      samplePubs = new Array(numOfPubs);
-      //Run publishing simulation
-      for(let i=0; i<numOfPubs; i++){
-        let pubTopic = generateTopic(4, pubTopicLevel);
-        samplePubs[i] = new Publisher(`Publ ${i}`, pubInterval, clientId, pubTopic);
+    //Run subscribing simulation
+    setTimeout(() => {
+      let alreadyPublishedTopics = Client.allPublishedTopics();
+      // console.log("alllllllrrreeeeeeaaaadyyyyy", alreadyPublishedTopics)
+      for (let j = 0; j < numOfSubs; j++) {
+        let subTopic =
+          alreadyPublishedTopics[
+            getRandomNumber(0, alreadyPublishedTopics.length)
+          ]; //Get a random topic from already published for the simulation
+        sampleSubs[j] = new Subscribe(`Subl ${j}`, clientId, subTopic);
+        // console.log(sampleSubs[j]);
       }
+      //Emit subscribed topics
+      client.emit("topics", Client.allPublishedTopics());
+    }, 4000);
 
-      //Run subscribing simulation
-      setTimeout(() => {
-        let alreadyPublishedTopics = Client.allPublishedTopics();
-        // console.log("alllllllrrreeeeeeaaaadyyyyy", alreadyPublishedTopics)
-        for(let j=0; j<numOfSubs; j++){
-          let subTopic = alreadyPublishedTopics[getRandomNumber(0, alreadyPublishedTopics.length)]; //Get a random topic from already published for the simulation
-          sampleSubs[j] = new Subscribe(`Subl ${j}`, clientId, subTopic);
-          // console.log(sampleSubs[j]);
-        }
-        //Emit subscribed topics
-        client.emit("topics", Client.allPublishedTopics());
-      }, 4000);
+    let receivedMessagesCount = 0;
+    //listen for messages
+    const listenForMsgs = Client.getClient(clientId);
+    listenForMsgs.on("message", (topic, message) => {
+      // console.log("The Topic" + topic + ":" + message);
+      receivedMessagesCount += 1;
+      client.emit("received", `${receivedMessagesCount}`);
+    });
 
-      let receivedMessagesCount = 0;
-      //listen for messages
-      const listenForMsgs = Client.getClient(clientId);
-      listenForMsgs.on('message', (topic, message) => {
-        // console.log("The Topic" + topic + ":" + message);
-        receivedMessagesCount += 1;
-        client.emit("received", `${receivedMessagesCount}`);
-      });
-
-      //send sample statistics
-     let statsInterval = setInterval(() => {
-      symbs = Object.getOwnPropertySymbols(client.conn.transport.socket._socket);
+    //send sample statistics
+    let statsInterval = setInterval(() => {
+      symbs = Object.getOwnPropertySymbols(
+        client.conn.transport.socket._socket
+      );
       bytesRead = client.conn.transport.socket._socket[symbs[11]];
-      bytesWritten = client.conn.transport.socket._socket[symbs[12]]; 
-      client.emit("memory-usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
-      client.emit("cpu-usage", `${((osu.cpu.loadavgTime() / 2) * 10).toFixed(2)} %`);
+      bytesWritten = client.conn.transport.socket._socket[symbs[12]];
+      client.emit(
+        "memory-usage",
+        `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
+      );
+      client.emit(
+        "cpu-usage",
+        `${((osu.cpu.loadavgTime() / 2) * 10).toFixed(2)} %`
+      );
       client.emit("sent", `${Client.messageCount[clientId]}`);
       // console.log(client.conn.transport.socket._socket[symbs[12]], symbs[12], symbs[11]);
       client.emit("netin", `${bytesRead}`);
       client.emit("netout", `${bytesWritten}`);
-     }, 2000)
-     })
+    }, 2000);
+  });
 
+  client.on("stopSimulation", (data) => {
+    //clear interval function when simulation stops
+    const { numOfPubs } = data;
+    if (samplePubs) {
+      for (let i = 0; i < numOfPubs; i++) {
+        // console.log(numOfPubs);
+        // console.log(Object.keys(samplePubs));
+        samplePubs[i].stopPublishing(samplePubs[i].intervalId);
+        // console.log(samplePubs[i].intervalId);
+      }
+    }
+    samplePubs = null;
+    Client.messageCount[clientId] = 0;
+    clearInterval(statsInterval);
+  });
 
-    client.on('stopSimulation', (data) => {
-      //clear interval function when simulation stops
-      const {numOfPubs} = data;
-          if(samplePubs){
-            for(let i=0; i<numOfPubs; i++){
-              // console.log(numOfPubs);
-              // console.log(Object.keys(samplePubs));
-              samplePubs[i].stopPublishing(samplePubs[i].intervalId);
-              // console.log(samplePubs[i].intervalId);
-              }
-          }
-            samplePubs = null;
-            Client.messageCount[clientId] = 0;
-            clearInterval(statsInterval);
-    })
-
-    client.on('disconnect', () => { 
-      console.log("User Disconnected");
-     });
+  client.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
 });
-server.listen(80);
+server.listen(9000);
 
 app.listen(Port, () => {
   console.log("App Running...");
