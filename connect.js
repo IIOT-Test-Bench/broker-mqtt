5f7af2ade4b340f8985641b74182951a2438869e
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-09-15 13:09:45 +0100

    Update connect.js

--- connect.js
+++ connect.js
@@ -19,7 +19,7 @@ const io = require("socket.io")(server, {
     origin: "https://iiot-test-bench-project.netlify.app",
     credentials: true,
   },
-}).listen(server);
+});
 
 const cors = require("cors");
 //import subscriber class
