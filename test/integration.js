3966ad3a2ffd51c6c339e710f8c224dc53d29d51
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-09-16 05:13:56 +0000

    updated test

--- test/integration.js
+++ test/integration.js
@@ -1,3 +1,9 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const assert = chai.assert;
+const Client = require("../Classes/Client");
+
+const { app } = require("../connect");
+chai.use(chaiHttp);
+
+describe("Integration Test", () => {});
